b1c7907b91680877a8471f06a18f14a8e53e0bb7
Ryz <mirco.heitmann@gmail.com>
2020-02-28 22:34:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const puppeteer = require('puppeteer-extra');
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+puppeteer.use(AdblockerPlugin())
 
 var readline = require('readline');
 
@@ -19,12 +21,23 @@ function input(prompt) {
 }
 
 (async () => {
-var n = await input("Number: ");
+  var email = 'captchascore1@gmail.com';//await input('email bidde:');
+  var password = 'neversettleforless';//await input('passwort bidde:');
+
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
+
   await page.goto('https://snipes.com/login', {waitUntil: 'networkidle2'});
-  await page.type('#searchbox input', 'Headless Chrome');
-  //await page.screenshot({path: 'screen.jpg', fullPage: true});
+  await page.type('input[type=email]', email);
+  await page.type('input[type=password]', password + '\n');
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  console.log('logged in');
+
+  //await page.evaluate(async() => { window.scrollBy(0, 10); });
+
+  await page.goto('https://www.snipes.com/p/jordan-air_jordan_1_mid-black%2Fpine_green%2Fwhite%2Fgym_red-00013801788237.html?chosen=size&dwvar_00013801126425_212=45', {waitUntil: 'networkidle2'});
+  await page.click('.f-pdp-button.f-pdp-button--active.js-btn-add-to-cart');
+
   await page.waitFor(5000);
   await browser.close();
 })();
